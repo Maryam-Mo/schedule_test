dceff0778f979d8310337e73eb9a1745be5cdd8c
Maryam <Maryam_moein@hotmail.com>
2020-02-18 13:15:13 +0800

    Implement addADayToList method

--- src/main/java/Schedule.java
+++ src/main/java/Schedule.java
@@ -6,67 +6,58 @@ import java.util.*;
 
 public class Schedule {
     private static final String BY = "by";
-    private static final String ONE_AM_TEST = "1:00 AM EST";
-    private static final String NINE_AM_FIVE_PM_EST= "9:00 AM EST, 5:00 PM EST";
-    private static final String NINE_AM_EST= "9:00 AM EST";
-    private static final String FIVE_PM_EST= "5:00 PM EST";
-    private static final String EVERY_DAY= "every day";
-    private static final String EVERY_HOUR= "every hour";
-    private static final String EVERY_MONDAY_TO_FRIDAY= "every Mon-Fri";
-    private static final String EVERY_SUNDAY_TO_FRIDAY= "every Sun-Fri";
-    private static final String EVERY_MONDAY_WEDNESDAY_FRIDAY= "every Mon, Wed, Fri";
-    private static final String EVERY_FRIDAY= "every Fri";
-    private static final String EVERY_SIX_HOURS= "every 6 hours";
-    private static final String EVERY_THIRTY_MINUTES= "every 30 minutes";
-    private static final String EVERY_FIRST= "every 1st";
-    private static final String EVERY_FIFTEEN_THIRTY= "every 15th, 30th";
-    private static final String EVERY_TEN_TWELVE= "every 10th-12th";
-    private static final String EVERY_THIRTYFIRST_TO_THIRD= "every 31st-3rd";
-    private static final String EVERY_SECOND_TO_TWENTYSECOND= "every 2nd-22nd";
-    private static final String THIRTY_FIRST= "31st";
-    private static final String TWENTY_FIRST= "21st";
-    private static final String TWENTY_SECOND= "22nd";
-    private static final String FIFTEEN= "15th";
-    private static final String THIRTY= "30th";
-    private static final String FIRST= "1st";
-    private static final String SECOND= "2nd";
-    private static final String THIRD= "3rd";
-    private static final String TEN= "10th";
-    private static final String ELEVEN= "11th";
-    private static final String TWELVE= "12th";
-    private static final String THIRTY_MINUTES= "30 minutes";
-    private static final String SIX_HOURS= "6 hours";
-    private static final String MONDAY= "Mon";
-    private static final String TUESDAY= "Tue";
-    private static final String WEDNESDAY= "Wed";
-    private static final String THURSDAY= "Thu";
-    private static final String FRIDAY= "Fri";
-    private static final String SUNDAY= "Sun";
-    private static final String SATURDAY= "Saturday";
-    private static final String EVERY= "every";
+    private static final String EVERY = "every ";
+    private static final String DAY = "day";
+    private static final String HOUR = "hour";
 
     private List<Date> byList = new ArrayList<Date>();
-
-    private boolean everyDay;
-
-    private boolean everyHour;
-
     private List<String> everyList = new ArrayList<String>();
-    private List<String> dayList = Arrays.asList(SUNDAY, MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY);
-    private String scheduleString;
+    private boolean everyDay;
+    private boolean everyHour;
     private String[] split;
-
     private SimpleDateFormat simpleDateFormat;
+    private String scheduleString;
 
     public Schedule(String scheduleString) {
         this.scheduleString = scheduleString;
         split = scheduleString.split(EVERY);
         simpleDateFormat = new SimpleDateFormat("h:mm a z");
-        addSingleHourToListBy();
-        addMoreThanOneHourToList();
+        chooseEveryDay();
+        chooseEveryHour();
+        if (!split[0].isEmpty()) {
+            addAnHourToList();
+            addMoreThanOneHourToList();
+        }
+        if (!(split[1].equalsIgnoreCase(DAY) || split[1].equalsIgnoreCase(HOUR))) {
+            addADayToList();
+            addMoreThanOneDayToList();
+        }
     }
 
-    private void addSingleHourToListBy() {
+    private void chooseEveryHour() {
+        if (split[1].equalsIgnoreCase(HOUR)) {
+            everyHour = true;
+        }
+    }
+
+    private void chooseEveryDay() {
+        if (split[1].equalsIgnoreCase(DAY)) {
+            everyDay = true;
+        }
+    }
+
+    private void addADayToList() {
+        if (!split[1].contains(",") && !split[1].contains("-")){
+            everyList.clear();
+            everyList.add(split[1]);
+        }
+    }
+
+    private void addMoreThanOneDayToList() {
+
+    }
+
+    private void addAnHourToList() {
         if (!split[0].contains(",")){
             split[0] = split[0].substring(3,split[0].length()-1);
             byList.clear();
@@ -80,12 +71,12 @@ public class Schedule {
 
     private void addMoreThanOneHourToList(){
         if (split[0].contains(",")){
-            split = split[0].split(", ");
-            split[0] = split[0].substring(3);
-            int size = (split.length) - 1;
-            split[size] = split[size].substring(0, split[size].length()-1);
+            String[] splitHour = split[0].split(", ");
+            splitHour[0] = splitHour[0].substring(3);
+            int size = (splitHour.length) - 1;
+            splitHour[size] = splitHour[size].substring(0, splitHour[size].length()-1);
             byList.clear();
-            for (String s: split){
+            for (String s: splitHour){
                 try {
                     byList.add(simpleDateFormat.parse(s));
                 } catch (ParseException e) {
@@ -107,21 +98,14 @@ public class Schedule {
     }
 
     public boolean isEveryDay() {
-//        if (scheduleString.contains(EVERY_DAY)) {
-//            return true;
-//        }
-        return false;
+        return everyDay;
     }
 
     public boolean isEveryHour() {
-//        if (scheduleString.contains(EVERY_HOUR)) {
-//            return true;
-//        }
-        return false;
+        return everyHour;
     }
 
     public List<String> getEveryList() {
-
-        return null;
+        return everyList;
     }
 }
--- src/test/java/ScheduleTest.java
+++ src/test/java/ScheduleTest.java
@@ -16,111 +16,111 @@ public class ScheduleTest {
     @Test
     public void testEveryDay() {
         Schedule schedule = new Schedule("every day");
-//        assertSchedule(schedule, false, true, false, null, null);
+        assertSchedule(schedule, false, true, false, null, null);
     }
 
-    @Test
-    public void testEveryMonToFri() {
-        Schedule schedule = new Schedule("every Mon-Fri");
+//    @Test
+//    public void testEveryMonToFri() {
+//        Schedule schedule = new Schedule("every Mon-Fri");
 //        assertSchedule(schedule, false, false, false, null, "Mon,Tue,Wed,Thu,Fri");
-    }
+//    }
 
-    @Test
-    public void testEveryMonWedFri() {
-        Schedule schedule = new Schedule("every Mon, Wed, Fri");
+//    @Test
+//    public void testEveryMonWedFri() {
+//        Schedule schedule = new Schedule("every Mon, Wed, Fri");
 //        assertSchedule(schedule, false, false, false, null, "Mon,Wed,Fri");
-    }
+//    }
 
     @Test
     public void testEveryHour() {
         Schedule schedule = new Schedule("every hour");
-//        assertSchedule(schedule, false, false, true, null, null);
+        assertSchedule(schedule, false, false, true, null, null);
     }
 
     @Test
     public void testEvery6Hours() {
         Schedule schedule = new Schedule("every 6 hours");
-//        assertSchedule(schedule, false, false, false, null, "6 hours");
+        assertSchedule(schedule, false, false, false, null, "6 hours");
     }
 
     @Test
     public void testEvery30Minutes() {
         Schedule schedule = new Schedule("every 30 minutes");
-//        assertSchedule(schedule, false, false, false, null, "30 minutes");
+        assertSchedule(schedule, false, false, false, null, "30 minutes");
     }
 
     @Test
     public void testBy1AmEveryDay() {
         Schedule schedule = new Schedule("by 1:00 AM EST every day");
-//        assertSchedule(schedule, true, true, false, "1:00 AM EST", null);
+        assertSchedule(schedule, true, true, false, "1:00 AM EST", null);
     }
 
     @Test
     public void testBy1AmEveryFri() {
         Schedule schedule = new Schedule("by 1:00 AM EST every Fri");
-//        assertSchedule(schedule, true, false, false, "1:00 AM EST", "Fri");
+        assertSchedule(schedule, true, false, false, "1:00 AM EST", "Fri");
     }
 
-    @Test
-    public void testBy1AmEveryMonToFri() {
-        Schedule schedule = new Schedule("by 1:00 AM EST every Mon-Fri");
+//    @Test
+//    public void testBy1AmEveryMonToFri() {
+//        Schedule schedule = new Schedule("by 1:00 AM EST every Mon-Fri");
 //        assertSchedule(schedule, true, false, false, "1:00 AM EST", "Mon,Tue,Wed,Thu,Fri");
-    }
+//    }
 
-    @Test
-    public void testBy1AmEveryMonWedFri() {
-        Schedule schedule = new Schedule("by 1:00 AM EST every Mon, Wed, Fri");
+//    @Test
+//    public void testBy1AmEveryMonWedFri() {
+//        Schedule schedule = new Schedule("by 1:00 AM EST every Mon, Wed, Fri");
 //        assertSchedule(schedule, true, false, false, "1:00 AM EST", "Mon,Wed,Fri");
-    }
+//    }
 
     @Test
     public void testBy1AmEvery1st() {
         Schedule schedule = new Schedule("by 1:00 AM EST every 1st");
-//        assertSchedule(schedule, true, false, false, "1:00 AM EST", "1st");
+        assertSchedule(schedule, true, false, false, "1:00 AM EST", "1st");
     }
 
-    @Test
-    public void testBy1AmEvery15th30th() {
-        Schedule schedule = new Schedule("by 1:00 AM EST every 15th, 30th");
+//    @Test
+//    public void testBy1AmEvery15th30th() {
+//        Schedule schedule = new Schedule("by 1:00 AM EST every 15th, 30th");
 //        assertSchedule(schedule, true, false, false, "1:00 AM EST", "15th,30th");
-    }
+//    }
 
-    @Test
-    public void testBy1AmEvery10thTo12th() {
-        Schedule schedule = new Schedule("by 1:00 AM EST every 10th-12th");
+//    @Test
+//    public void testBy1AmEvery10thTo12th() {
+//        Schedule schedule = new Schedule("by 1:00 AM EST every 10th-12th");
 //        assertSchedule(schedule, true, false, false, "1:00 AM EST", "10th,11th,12th");
-    }
+//    }
 
-    @Test
-    public void testBy1AmEvery31stTo3rd() {
-        Schedule schedule = new Schedule("by 1:00 AM EST every 31st-3rd");
+//    @Test
+//    public void testBy1AmEvery31stTo3rd() {
+//        Schedule schedule = new Schedule("by 1:00 AM EST every 31st-3rd");
 //        assertSchedule(schedule, true, false, false, "1:00 AM EST", "31st,1st,2nd,3rd");
-    }
+//    }
 
-    @Test
-    public void testBy1AmEvery2ndTo22nd() {
-        Schedule schedule = new Schedule("by 1:00 AM EST every 2nd-22nd");
+//    @Test
+//    public void testBy1AmEvery2ndTo22nd() {
+//        Schedule schedule = new Schedule("by 1:00 AM EST every 2nd-22nd");
 //        assertSchedule(schedule, true, false, false, "1:00 AM EST",
 //                "2nd,3rd,4th,5th,6th,7th,8th,9th,10th,11th,12th,13th,14th,15th,16th,17th,18th,19th,20th,21st,22nd");
-    }
+//    }
 
-    @Test
-    public void testBy9Am5PmEverySunToFri() {
-        Schedule schedule = new Schedule("by 9:00 AM EST, 5:00 PM EST every Sun-Fri");
+//    @Test
+//    public void testBy9Am5PmEverySunToFri() {
+//        Schedule schedule = new Schedule("by 9:00 AM EST, 5:00 PM EST every Sun-Fri");
 //        assertSchedule(schedule, true, false, false, "9:00 AM EST,5:00 PM EST", "Sun,Mon,Tue,Wed,Thu,Fri");
-    }
+//    }
 
-    @Test
-    public void testBy9Am5PmEveryMonToFri() {
-        Schedule schedule = new Schedule("by 9:00 AM EST, 5:00 PM EST every Mon-Fri");
+//    @Test
+//    public void testBy9Am5PmEveryMonToFri() {
+//        Schedule schedule = new Schedule("by 9:00 AM EST, 5:00 PM EST every Mon-Fri");
 //        assertSchedule(schedule, true, false, false, "9:00 AM EST,5:00 PM EST", "Mon,Tue,Wed,Thu,Fri");
-    }
+//    }
 
-    @Test
-    public void testBy9Am5Pm1amEveryMonToFri() {
-        Schedule schedule = new Schedule("by 1:00 AM EST, 9:00 AM EST, 5:00 PM EST every Mon-Fri");
+//    @Test
+//    public void testBy9Am5Pm1amEveryMonToFri() {
+//        Schedule schedule = new Schedule("by 1:00 AM EST, 9:00 AM EST, 5:00 PM EST every Mon-Fri");
 //        assertSchedule(schedule, true, false, false, "9:00 AM EST,5:00 PM EST", "Mon,Tue,Wed,Thu,Fri");
-    }
+//    }
 
     @Test
     public void testBy9Am5Pm1am() {
